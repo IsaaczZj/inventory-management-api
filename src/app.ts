209bee486238f5b3a13909ab87078033ef8647f4
isaac_dev <joseisaac.isaac123@gmail.com>
2026-04-16 13:29:39 -0300

    feat: creating product and category modeling

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, Router } from "express";
+import cors from "cors";
+import { router } from "./routes";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", router);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
-import express, { Request, Response, NextFunction } from 'express';
-import cors from 'cors';
-import router from './routes';
+import { app } from "./app";
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use('/api', router);
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
+  console.log(`Servidor rodando em na porta ${PORT}`);
+  console.log(`Ambiente: ${process.env.NODE_ENV} || development`);
 });
